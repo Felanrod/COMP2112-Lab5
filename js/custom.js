b93a41b6a0e2440602cc664e78c81c0bcdc28c14
Joel Murphy <j.murphy2@mycanadore.ca>
2018-10-07 23:57:04 -0400

    Lab 5 Completed

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+// Custom JavaScript
+//Just keeping it in here for future reference of carousel components
+// $('.carousel').carousel({
+//     interval: 3000,
+//     pause: false,
+//     keyboard: false
+// })
